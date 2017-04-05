5d5c1cffb8d3f997e758f48334b64b3b21bae400
antong <antong@netvertise.com>
2017-04-05 21:26:16 +0300

    adding the google maps variant

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -17,3 +17,19 @@ class GoogleMap extends Component {
 }
 
 export default GoogleMap
+
+// or
+
+//
+// import React from 'react'
+//
+// import { GoogleMapLoader, GoogleMap } from 'react-google-maps'
+//
+// export default (props) => {
+//   return (
+//     <GoogleMapLoader
+//       container={<div style={{height: '100%'}} />}
+//       googleMapElement={<GoogleMap defaultZoom={12} defaultCenter={{lat: props.lat,lng: props.lon}} />
+//       }
+//   )
+// }
